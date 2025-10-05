43bf8dcf3c2ae8cd02ed3cf5ec76ea455a68678c
121sankalp <110221095+121sankalp@users.noreply.github.com>
2025-10-05 23:24:24 +0530

    Project completion

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltRoutesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AltRoutesApplication.class, args);
+    }
+}
